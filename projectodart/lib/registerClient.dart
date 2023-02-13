6e36f4ec81d223ba9635aa7689ba8a1bd7cb0af1
luisvapa2012 <luisvapa2012@gmail.com>
2023-02-13 23:42:55 +0100

    13/02/23

--- projectodart/lib/registerClient.dart
+++ projectodart/lib/registerClient.dart
@@ -2,12 +2,49 @@ import 'package:flutter/material.dart';
 
 class RegisterClient extends StatefulWidget {
 
+  @override
+  State<StatefulWidget> createState() => _RegisterCliente();
+
 
 
 }
   
 class _RegisterCliente extends State<RegisterClient>{
 
+  late TextEditingController controllerNombre;
+  late TextEditingController controllerProblema;
+  late TextEditingController controllerDescripcion;
+  late TextEditingController controllerFecha;
+  late TextEditingController controllerEstado;
+  late TextEditingController controllerTelefono;
 
-  
+  @override
+  void initState(){
+    
+    controllerNombre = new TextEditingController();
+    controllerProblema = new TextEditingController();
+    controllerDescripcion =  new TextEditingController();
+    controllerFecha = new TextEditingController();
+    controllerEstado = new TextEditingController();
+    controllerTelefono = new TextEditingController();
+    super.initState();
+  }
+
+
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Registro de Cliente'),
+      ),
+      body: ListView(),
+
+    );
+
+
+  }
+
+
+
 }
