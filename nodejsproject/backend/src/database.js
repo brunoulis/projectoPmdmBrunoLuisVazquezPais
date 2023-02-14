47e80b5c54285011e175cc6ebc4951b4ea081982
luisvapa2012 <luisvapa2012@gmail.com>
2023-02-14 13:29:03 +0100

    14/02/23

--- nodejsproject/backend/src/database.js
+++ nodejsproject/backend/src/database.js
@@ -3,11 +3,7 @@ const dbConection= async() =>{
     try{
         console.log('Conectando DB.........');
         // Creamos la conexion a la base de datos mongodb con contraseña y usuario root
-        await mongoose.connect('mongodb://root:root@127.0.0.1:27017/servicio', {
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useNewUrlParser: true,
-            useUnifiedTopology: true
+        await mongoose.connect(`mongodb://root:root@127.0.0.1:27017/servicio?authSource=admin`, {
         });
         console.log('Conectado a DB');
     }catch (error){
