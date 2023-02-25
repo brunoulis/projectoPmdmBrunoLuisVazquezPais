e21583adaf156c8b899e1dd4ead70cbe7664147c
luisvapa2012 <luisvapa2012@gmail.com>
2023-02-25 20:37:24 +0100

    25/02/23

--- projectodart/lib/pantallas/registerClient.dart
+++ projectodart/lib/pantallas/registerClient.dart
@@ -4,98 +4,123 @@ import 'package:projectodart/modelos/cliente.model.dart';
 import 'package:projectodart/pantallas/textbox.dart';
 import 'package:projectodart/peticiones/cliente.peticion.dart';
 import 'package:flutter/services.dart';
-// Importamos el paquete flutter_masked_text para poder usar el controlador MaskedTextController
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 class RegisterClient extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _RegisterCliente();
 }
-  
-class _RegisterCliente extends State<RegisterClient>{
 
+class _RegisterCliente extends State<RegisterClient> {
   late TextEditingController controllerNombre;
   late TextEditingController controllerProblema;
   late TextEditingController controllerDescripcion;
   late TextEditingController controllerFecha;
   late TextEditingController controllerEstado;
-  late MaskedTextController controllerTelefono;
+  late TextEditingController controllerTelefono;
 
   final DateFormat formatter = DateFormat('dd/MM/yy');
 
   @override
-  void initState(){
-    
-    controllerNombre = new TextEditingController();
-    controllerProblema = new TextEditingController();
-    controllerDescripcion =  new TextEditingController();
-    controllerFecha = new TextEditingController();
-    controllerEstado = new TextEditingController();
-    controllerTelefono = new MaskedTextController(mask: '000-000-0000');
+  void initState() {
+    controllerNombre = TextEditingController();
+    controllerProblema = TextEditingController();
+    controllerDescripcion = TextEditingController();
+    controllerFecha = TextEditingController();
+    controllerEstado = TextEditingController();
+    controllerTelefono = TextEditingController();
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Registro de Cliente'),
       ),
-      body: ListView(
-        children: [
-          TextBox(controllerNombre, 'Nombre'),
-          TextBox(controllerProblema, 'Problema'),
-          TextBox(controllerDescripcion, 'Descripcion'),
-          TextFormField(
-            controller: controllerFecha,
-            decoration: InputDecoration(
-              labelText: 'Fecha',
-              hintText: 'dd/mm/aa'
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16.0),
+        child: ListView(
+          children: [
+            TextBox(controllerNombre, 'Nombre'),
+            TextBox(controllerProblema, 'Problema'),
+            TextBox(controllerDescripcion, 'Descripcion'),
+            TextFormField(
+              controller: controllerFecha,
+              decoration: InputDecoration(
+                labelText: 'Fecha',
+                hintText: 'dd/mm/aa',
+              ),
+              onTap: () async {
+                DateTime? picked = await showDatePicker(
+                    context: context,
+                    initialDate: DateTime.now(),
+                    firstDate: DateTime(1900),
+                    lastDate: DateTime(2100));
+                if (picked != null) {
+                  String formattedDate = formatter.format(picked);
+                  controllerFecha.text = formattedDate;
+                }
+              },
             ),
-            onTap: () async {
-              DateTime? picked = await showDatePicker(
-                  context: context,
-                  initialDate: DateTime.now(),
-                  firstDate: DateTime(1900),
-                  lastDate: DateTime(2100)
-              );
-              if (picked != null) {
-                String formattedDate = formatter.format(picked);
-                controllerFecha.text = formattedDate;
-              }
-            },
-          ),
-          TextBox(controllerEstado, 'Estado'),
-          TextFormField(
-            controller: controllerTelefono,
-            keyboardType: TextInputType.phone,
-            decoration: InputDecoration(
-              labelText: 'Telefono',
-            ),
-          ),
-          ElevatedButton(
-            onPressed: (){
-              String nombre = controllerNombre.text;
-              String problema = controllerProblema.text;
-              String descripcion = controllerDescripcion.text;
-              String fecha = controllerFecha.text;
-              String estado = controllerEstado.text;
-              String telefono = controllerTelefono.text;
-
-              if(nombre.isNotEmpty && problema.isNotEmpty  && descripcion.isNotEmpty  && fecha.isNotEmpty  && estado.isNotEmpty  && telefono.isNotEmpty ){
-                
-                Client client = Client.fromData(nombre, problema, descripcion, fecha, estado, telefono);
-                addClient(client).then((value) {
-                  if(value.id!=''){
-                    Navigator.pop(context, value);
+            SizedBox(height: 16),
+            TextBox(controllerEstado, 'Estado'),
+            SizedBox(height: 16),
+            TextFormField(
+              controller: controllerTelefono,
+              keyboardType: TextInputType.phone,
+              decoration: InputDecoration(
+                labelText: 'Telefono',
+              ),
+              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              onChanged: (value) {
+                setState(() {
+                  if (value.isNotEmpty) {
+                    controllerTelefono.text = formatPhoneNumber(value);
+                    controllerTelefono.selection = TextSelection.fromPosition(
+                        TextPosition(offset: controllerTelefono.text.length));
                   }
                 });
-              }
-            },
-            child: Text('Guardar'),
-          )
-        ],
+              },
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                String nombre = controllerNombre.text;
+                String problema = controllerProblema.text;
+                String descripcion = controllerDescripcion.text;
+                String fecha = controllerFecha.text;
+                String estado = controllerEstado.text;
+                String telefono = controllerTelefono.text;
+
+                if (nombre.isNotEmpty &&
+                    problema.isNotEmpty &&
+                    descripcion.isNotEmpty &&
+                    fecha.isNotEmpty &&
+                    estado.isNotEmpty &&
+                    telefono.isNotEmpty) {
+                  Client client =
+                      Client.fromData(nombre, problema, descripcion, fecha, estado, telefono);
+                  addClient(client).then((value) {
+                    if (value.id != '') {
+                      Navigator.pop(context, value);
+                    }
+                  });
+                }
+              },
+              child: Text('Guardar'),
+            ),
+          ],
+        ),
       ),
     );
+  }
+
+  String formatPhoneNumber(String value) {
+    final phoneNumber = value.replaceAll(RegExp(r'[^\d]'), '');
+    final firstSubstring =
+        phoneNumber.substring(0, phoneNumber.length > 3 ? 3 : phoneNumber.length);
+    final secondSubstring = phoneNumber.length > 3 ? phoneNumber.substring(3) : '';
+
+    return '$firstSubstring-$secondSubstring';
   }
 }
